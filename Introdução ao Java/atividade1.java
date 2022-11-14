48cd0e9a48fb1008cc55347d37df78602f12f82b
gabrielilb <69219473+gabrielilb@users.noreply.github.com>
2022-11-13 21:50:58 -0300

    Create atividade1.java

---
+++ Introdução ao Java/atividade1.java
@@ -0,0 +1,9 @@
+public class atividade1 {
+    public static void main(String[] args) {
+      
+       System.out.println("Programação I");
+       System.out.println("\t- Professora: Andressa Sebben \n\t- Aluna: Gabrieli \n\t- Objetivo:");
+       System.out.println("\t\tO objetivo dessa disciplina é compreender os conceitos fundamentais \n\t\tdo paradigma de programação orientada a objetos e aplicá-los no \n\t\tdesenvolvimento de soluções de software.");
+       System.out.println(" \t- Turno: Noturno");
+    }
+}
